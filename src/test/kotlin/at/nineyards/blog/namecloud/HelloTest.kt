ddf2e0682a28b3590dd8bb7fb15b9e99404fac18
Alois Wollersberger <alois.wollersberger@9yards.at>
2018-02-22 15:25:58 +0100

    added test

---
+++ src/test/kotlin/at/nineyards/blog/namecloud/HelloTest.kt
@@ -0,0 +1,22 @@
+package at.nineyards.blog.namecloud
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloTest{
+    @Autowired
+    lateinit var restTemplate : TestRestTemplate
+
+    @Test
+    fun helloTest(){
+        val body = restTemplate.getForObject("/hello",String::class.java)
+        assertThat(body).isEqualTo("Hi There")
+    }
+}
